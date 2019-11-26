ff46cbd5bc18c8cd6a17ce4f8adcf974fffbcc2f
Pavel Strelchenko <p.strelchenko@hh.ru>
2019-11-26 19:56:09 +0300

    Add stack trace into test result bundle

--- kaspresso/src/main/kotlin/com/malinsky/marathon/core/steps/StepsResultsConsumerImpl.kt
+++ kaspresso/src/main/kotlin/com/malinsky/marathon/core/steps/StepsResultsConsumerImpl.kt
@@ -3,6 +3,8 @@ package com.malinsky.marathon.core.steps
 import android.os.Bundle
 import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_KEY_NAME_CLASS
 import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_KEY_NAME_TEST
+import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_KEY_NUM_TOTAL
+import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_KEY_STACK
 import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_VALUE_RESULT_ASSUMPTION_FAILURE
 import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_VALUE_RESULT_FAILURE
 import androidx.test.internal.runner.listener.InstrumentationResultPrinter.REPORT_VALUE_RESULT_OK
@@ -13,6 +15,8 @@ import com.kaspersky.kaspresso.testcases.models.extensions.toTestIdentifier
 import org.junit.AssumptionViolatedException
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import java.io.PrintWriter
+import java.io.StringWriter
 
 /**
  * This [org.junit.rules.TestWatcher] writes information about test steps into adb shell's output
@@ -29,6 +33,9 @@ class StepsResultsConsumerImpl(
     companion object {
         private const val INSTRUMENTATION_KEY_STEPS_RESULTS_JSON = "marathon.stepsResultsJson"
         private const val EMPTY_STEPS_RESULTS_JSON = "[]"
+
+        // copied from InstrumentationResultPrinter.MAX_TRACE_SIZE
+        private const val MAX_TRACE_SIZE = 32 * 1024
     }
 
     private var stepsResultsJson = EMPTY_STEPS_RESULTS_JSON
@@ -43,7 +50,7 @@ class StepsResultsConsumerImpl(
 
         val testIdentifier = description.toTestIdentifier()
         uiTestLogger.d("$testIdentifier failed")
-        sendStatus(REPORT_VALUE_RESULT_FAILURE, description)
+        sendStatus(REPORT_VALUE_RESULT_FAILURE, description, e)
     }
 
     override fun skipped(e: AssumptionViolatedException, description: Description) {
@@ -51,7 +58,7 @@ class StepsResultsConsumerImpl(
 
         val testIdentifier = description.toTestIdentifier()
         uiTestLogger.d("$testIdentifier skipped")
-        sendStatus(REPORT_VALUE_RESULT_ASSUMPTION_FAILURE, description)
+        sendStatus(REPORT_VALUE_RESULT_ASSUMPTION_FAILURE, description, e)
     }
 
     override fun succeeded(description: Description) {
@@ -62,16 +69,33 @@ class StepsResultsConsumerImpl(
         sendStatus(REPORT_VALUE_RESULT_OK, description)
     }
 
-    private fun sendStatus(code: Int, description: Description) {
+    private fun sendStatus(code: Int, description: Description, throwable: Throwable? = null) {
         InstrumentationRegistry.getInstrumentation()
-            .sendStatus(code, createBundle(description, stepsResultsJson))
+            .sendStatus(code, createBundle(description, stepsResultsJson, throwable))
     }
 
-    private fun createBundle(description: Description, stepsResultsJson: String): Bundle {
+    private fun createBundle(
+        description: Description,
+        stepsResultsJson: String,
+        throwable: Throwable?
+    ): Bundle {
         return Bundle().apply {
             putString(REPORT_KEY_NAME_CLASS, description.className)
             putString(REPORT_KEY_NAME_TEST, description.methodName)
+            putInt(REPORT_KEY_NUM_TOTAL, description.testCount())
+            throwable?.let { putString(REPORT_KEY_STACK, throwable.getTrace()) }
             putString(INSTRUMENTATION_KEY_STEPS_RESULTS_JSON, stepsResultsJson)
         }
     }
+
+
+    private fun Throwable.getTrace(): String {
+        val stringWriter = StringWriter()
+        val writer = PrintWriter(stringWriter)
+        printStackTrace(writer)
+        val trace = stringWriter.toString()
+
+        return trace.takeIf { it.length < MAX_TRACE_SIZE } ?: trace.substring(0, MAX_TRACE_SIZE)
+    }
+
 }
